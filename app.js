a535047abee7415617459ab5e2183137351fef3e
FrederikBRoth <frboe17@student.sdu.dk>
2020-10-13 18:47:58 +0200

    removed some fucking function liek an idiot

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const session = require("express-session")
-
+const {assempleBirthdayGreeting} = require("./birthdaybash")
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
