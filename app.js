cd8450f550df0de7ce0452b9519b0148ae1a745e
FrederikBRoth <frboe17@student.sdu.dk>
2020-10-13 16:52:11 +0200

    Added store to session

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const session = require("express-session")
+const redis = require('redis')
 const { assempleBirthdayGreeting } = require("./birthdaybash")
+
 require('dotenv').config()
 
+let RedisStore = require('connect-redis')(session)
+let redisClient = redis.createClient()
 
 app.use(cors())
 app.use(express.json())
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60000, secure: false },
+    saveUninitialized: true,
+    cookie: { maxAge: 60000*60*8, secure: false },
+    store: new RedisStore({client: redisClient})
 }))
 app.use("/api/song", express.static('Output'));
 
