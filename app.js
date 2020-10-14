d0b6a074e535621d47bdfdfea199b1b16a088523
FrederikBRoth <frboe17@student.sdu.dk>
2020-10-14 23:33:14 +0200

    added download GET

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.use(session({
 }))
 app.use("/api/song", express.static('Output'));
 
+app.get("/api/download", (req, res) => {
+    const file = `${__dirname}/Output/${req.body.fileName}`
+    res.download(file)
+})
+
 app.post("/api/post", async (req, res) => {
     console.log(req.sessionID)
     console.log(req.body)
